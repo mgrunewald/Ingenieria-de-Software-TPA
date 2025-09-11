272d5185bf9ad91f00f96c732adfe54abf23a75e
mgrunewald <titigrune@gmail.com>
2025-09-11 13:49:57 -0300

    creo el springboot project

---
+++ TPA/src/main/java/org/udesa/tpa/TpaApplication.java
@@ -0,0 +1,13 @@
+package org.udesa.tpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpaApplication.class, args);
+    }
+
+}
---
+++ TPA/src/test/java/org/udesa/tpa/TpaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.udesa.tpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
