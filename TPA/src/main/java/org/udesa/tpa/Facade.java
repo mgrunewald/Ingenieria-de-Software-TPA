0212d4f2bf3421137db4d92161ebc448106fcf20
mgrunewald <titigrune@gmail.com>
2025-09-15 15:14:40 -0300

    4 test hechos

--- TPA/src/main/java/org/udesa/tpa/Facade.java
+++ TPA/src/main/java/org/udesa/tpa/Facade.java
@@ -2,9 +2,11 @@ package org.udesa.tpa;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 public class Facade {
     private final Map<String, String> users = new HashMap<>();
+    private final Map<String, String> sessions = new HashMap<>();
 
     public void register(String username, String password) {
         users.put(username, password); //creo la entrada en la la lista de (usuarios, contraseña)
@@ -12,5 +14,14 @@ public class Facade {
 
     public boolean exists(String username) {
         return users.containsKey(username);
+    }
+
+    public String login(String username, String password) {
+        String storedPassword = users.get(username);
+        if (storedPassword == null) { throw new IllegalArgumentException("Usuario no existe"); }
+        if (!storedPassword.equals(password)) { throw new IllegalArgumentException("Contraseña Incorrecta"); }
+        String token = UUID.randomUUID().toString();
+        sessions.put(token, username);
+        return token;
     }
 }
--- TPA/src/test/java/org/udesa/tpa/FacadeTest.java
+++ TPA/src/test/java/org/udesa/tpa/FacadeTest.java
@@ -11,15 +11,29 @@ public class FacadeTest {
     @Test
     void test01UnregisteredUsernameIsMissing() {
         facade = new Facade();
-        assertFalse(facade.exists("martina")); // todavia no registre mi usuario
+        assertFalse(facade.exists("martina"));
     }
 
     @Test
-    void test02RegisterUsernameCorrectly() {
+    void test02RegistersUsernameCorrectly() {
         facade = new Facade();
         facade.register("martina", "12345678");
         assertTrue(facade.exists("martina"));
     }
 
+    @Test
+    void test03FailsToLoginWithIncorrectPassword() {
+        facade = new Facade();
+        facade.register("martina", "12345678");
+        String token = facade.login("martina", "12345678");
+        assertThrows(IllegalArgumentException.class, () -> facade.login("martina", "otra"));
+    }
+
+    @Test
+    void test04UnregisteredUserFailsToLogin() {
+        Facade facade = new Facade();
+        assertThrows(IllegalArgumentException.class, () -> facade.login("maximo", "password123"));
+    }
 }
+
 
